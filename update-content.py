c478fe0e4fc3c242a2178d052bdc8116f7fac033
Greg Stretton <greg.stretton@protonmail.com>
2023-03-24 19:46:04 +0000

    add config

--- update-content.py
+++ update-content.py
@@ -2,17 +2,20 @@ import os
 import shutil
 import frontmatter
 from pathlib import Path
+import argparse
 
-obsidian_vault_path = "/home/greg/brain"
-quartz_content_path = "/home/greg/quartz/content"
+parser = argparse.ArgumentParser(description='Update Quartz content from Obsidian vault.')
+parser.add_argument('--vault', action="store", required=True, help='Obsidian vault path')
+parser.add_argument('--content', action="store", required=True, help='Quartz content path')
+parser.add_argument('--cleanup', action="store_true", default=False, help='Remove files from Quartz content that do not get copied')
+args = parser.parse_args()
+
+obsidian_vault_path = args.vault
+quartz_content_path = args.content
 included_dirs = [
 	'self',
 	'website-other',
 ]
-
-# remove everything in the destination directory
-if os.path.exists(quartz_content_path):
-    shutil.rmtree(quartz_content_path)
 
 # create Quartz content directory for Obsidian notes if it doesn't exist
 os.makedirs(quartz_content_path, exist_ok=True)
@@ -26,16 +29,26 @@ for root, dirs, files in os.walk(obsidian_vault_path):
 	# exclude directories listed in excluded_dirs
 	dirs[:] = [d for d in dirs if d in included_dirs]
 	
+	rel_obsidian_dir = os.path.relpath(root, obsidian_vault_path)
+	destination_dir = os.path.join(quartz_content_path, rel_obsidian_dir)
+
+	copied_files = []
 	for file in files:
 		if file.endswith('.md'):
 			abs_obsidian_path = os.path.join(root, file)
 			if is_published(abs_obsidian_path):
-				rel_obsidian_dir = os.path.relpath(root, obsidian_vault_path)
-				print("rel:", rel_obsidian_dir)
-
-				destination_dir = os.path.join(quartz_content_path, rel_obsidian_dir)
 				Path(destination_dir).mkdir(parents=True, exist_ok=True)
 
 				destination_path = os.path.join(destination_dir, file)
 				print("Copying " + abs_obsidian_path + " to " + destination_path)
 				shutil.copy2(abs_obsidian_path, destination_path)
+				copied_files.append(file)
+	
+	if args.cleanup:
+		# remove files that were not copied
+		for file in os.listdir(destination_dir):
+			if file not in copied_files:
+				file_path = os.path.join(destination_dir, file)
+				if os.path.isfile(file_path):
+					print("REMOVE:", file_path)
+					os.remove(file_path)
